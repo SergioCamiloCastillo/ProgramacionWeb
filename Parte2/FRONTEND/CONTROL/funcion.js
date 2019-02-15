1b72b0bf5b239aa31580ba91511c48b9947d8076
Sergui <scastillonu@uniminuto.edu.co>
2019-02-14 20:26:23 -0500

    Update funcion.js

--- Parte2/FRONTEND/CONTROL/funcion.js
+++ Parte2/FRONTEND/CONTROL/funcion.js
@@ -1,13 +1,12 @@
-var bEnviar = document.getElementById('boton');
+var bEnviar = document.getElementById('boton');//Aca se especifican los botones de añadir, eliminar y editar
 var bEliminar = document.getElementById('botonEliminar').addEventListener('click',eliminar);
 var bEditar = document.getElementById('botonEditar').addEventListener('click',editar);
 if (bEnviar) {
-    bEnviar.addEventListener('click', enviar);
+    bEnviar.addEventListener('click', enviar);//cuando el id de añadir recibe un click, se dirige a la funcion enviar
   function enviar() {
-        alert('voy a enviar');
         var nombre = document.getElementById('txtPrimerNombre').value;
         var apellido = document.getElementById('txtPrimerApellido').value;
-        var email = document.getElementById('txtEmail').value;
+        var email = document.getElementById('txtEmail').value;//Se crean las variables en javascript, de los ids de la pagina de html
         var tipoTarjeta = document.getElementById('txtTipoTarjeta').value;
         var titular = document.getElementById('txtTitular').value;
         var numeroTarjeta = document.getElementById('txtNumTarjeta').value;
@@ -22,34 +21,32 @@ if (bEnviar) {
 
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
+            if (this.readyState == 4 && this.status == 200) {//enviar los datos a traves de ajax
                 document.getElementById("demo").innerHTML = this.responseText;
             }
         };
         xhttp.open("GET", "../../BACKEND/CONTROL/cIndex.php?nombre=" + nombre + "&apellido=" + apellido + "&email=" + email + "&tipoTarjeta=" + tipoTarjeta +
             "&titular=" + titular + "&numeroTarjeta=" + numeroTarjeta + "&CVV2=" + CVV2 + "&fechaExpedicion=" + fechaExpedicion + "&direccion=" + direccion +
-            "&ciudad=" + ciudad + "&departamento=" + departamento + "&ZIP=" + ZIP + "&pais=" + pais + "&telefono=" + telefono, true);
+            "&ciudad=" + ciudad + "&departamento=" + departamento + "&ZIP=" + ZIP + "&pais=" + pais + "&telefono=" + telefono, true);//Enviamos los datos por GET a una pagina php, que seria el backend para que los procese
         xhttp.send();
 
     }
 } 
     
-    function eliminar() {
-        alert('voy a borrar');alert('voy a eliminar');
+    function eliminar() {//cuando el id de eliminar recibe un click, se dirige a la funcion eliminar
         var idEliminar = document.getElementById('txtEliminarID').value;
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
+            if (this.readyState == 4 && this.status == 200) {//enviar los datos a traves de ajax
                 document.getElementById("demo").innerHTML = this.responseText;
             }
         };
         xhttp.open("GET", "../../BACKEND/CONTROL/cIndex.php?idEliminar=" + idEliminar, true);
-        xhttp.send();
+        xhttp.send();//Enviamos los datos por GET a una pagina php, que seria el backend para que los procese
     }
 
    
-    function editar() {
-         alert('lolego a editar');
+    function editar() {//cuando el id de añadir recibe un click, se dirige a la funcion editar
         var editarID = document.getElementById('txtEditarID').value;
         var editarNombre = document.getElementById('txtEditarPrimerNombre').value;
         var editarApellido = document.getElementById('txtEditarPrimerApellido').value;
@@ -67,12 +64,12 @@ if (bEnviar) {
         var editarTelefono = document.getElementById('txtEditarTelefono').value;
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
+            if (this.readyState == 4 && this.status == 200) {//enviar los datos a traves de ajax
                 document.getElementById("demo").innerHTML = this.responseText;
             }
         };
         xhttp.open("GET", "../../BACKEND/CONTROL/cIndex.php?editarID=" + editarID + "&editarNombre=" + editarNombre + "&editarApellido=" + editarApellido + "&editarEmail=" + editarEmail +
             "&editarTipoTarjeta=" + editarTipoTarjeta + "&editarTitular=" + editarTitular + "&editarNumeroTarjeta=" + editarNumeroTarjeta + "&editarCVV2=" + editarCVV2 + "&editarFechaExpedicion=" + editarFechaExpedicion +
             "&editarDireccion=" + editarDireccion + "&editarCiudad=" + editarCiudad + "&editarDepartamento=" + editarDepartamento + "&editarZIP=" + editarZIP + "&editarPais=" + editarPais+ "&editarTelefono=" + editarTelefono, true);
-        xhttp.send();
+        xhttp.send();//Enviamos los datos por GET a una pagina php, que seria el backend para que los procese
     }
