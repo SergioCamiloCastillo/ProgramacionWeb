e2d6083d7350757ed16552e117f4a2e3b55574cd
Sergui <scastillonu@uniminuto.edu.co>
2019-02-14 20:26:17 -0500

    Update conexion.php

--- Parte2/BACKEND/MODEL/conexion.php
+++ Parte2/BACKEND/MODEL/conexion.php
@@ -1,15 +1,15 @@
 <?php
     class conectar{
-        
+        //Llegan los parametros enviados de la clase cIndex.php
         function conexion($nombre,$apellido,$email,$tipoTarjeta,$titular,$numeroTarjeta,$CVV2, $fechaExpedicion,$direccion,$ciudad,$telefono,$departamento,$ZIP,$pais){
            
             try{
                 $base=new PDO('mysql:host=localhost; dbname=programacionweb','root','');
-                $base->exec('SET CHARACTER SET UTF8');
+                $base->exec('SET CHARACTER SET UTF8');//Establecemos la conexion a la BD, teniendo en cuenta el nombre y la tabla en donde de alojaran los datos.
                 $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $sql ='insert into crud (Nombre, Apellido, Email, TipoTarjeta,Titular,NumeroTarjeta,CVV2,
                 FechaExpedicion, Direccion, Ciudad, Telefono, Departamento, ZIP, Pais) values(:nombre,:apellido,:email,:tipoTarjeta,:titular,:numeroTarjeta,:CVV2, :fechaExpedicion,:direccion,:ciudad,:telefono,:departamento,:ZIP,:pais)';
-                $resultado=$base->prepare($sql);
+                $resultado=$base->prepare($sql);//Añadiran los datos que lleguen
                 $resultado->execute(array(':nombre'=>$nombre,':apellido'=>$apellido,':email'=>$email,':tipoTarjeta'=>$tipoTarjeta,
                 ':titular'=>$titular,':numeroTarjeta'=>$numeroTarjeta,':CVV2'=>$CVV2,':fechaExpedicion'=>$fechaExpedicion,
                 ':direccion'=>$direccion,':ciudad'=>$ciudad,':telefono'=>$telefono,':departamento'=>$departamento,':ZIP'=>$ZIP,':pais'=>$pais));
