1fbb01023281d6a4a2363b994ab6438f5d958921
Sergui <scastillonu@uniminuto.edu.co>
2019-02-14 19:51:01 -0500

    Update conexion.php

--- Parte2/BACKEND/MODEL/conexion.php
+++ Parte2/BACKEND/MODEL/conexion.php
@@ -29,14 +29,38 @@
             
                 // sql to delete a record
                 $sql = "DELETE FROM crud WHERE id=$idEliminar";
+                
             
-                // use exec() because no results are returned
                 $conn->exec($sql);
                 echo "Record deleted successfully";
                 }
             catch(PDOException $e)
                 {
                 echo $sql . "<br>" . $e->getMessage();
+                }
+        }
+        function editar($editarID,$editarNombre,$editarApellido,$editarEmail,$editarTipoTarjeta,$editarTitular,$editarNumeroTarjeta, $editarCVV2,$editarFechaExpedicion,$editarDireccion,$editarCiudad,$editarDepartamento,$editarZIP,$editarPais,$editarTelefono){
+            try {
+                $conn = new PDO("mysql:host=localhost;dbname=programacionweb", 'root', '');
+                // set the PDO error mode to exception
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+                $sql = "UPDATE crud SET Nombre='$editarNombre', Apellido='$editarApellido', Email='$editarEmail', TipoTarjeta='$editarTipoTarjeta',
+                Titular='$editarTitular', NumeroTarjeta='$editarNumeroTarjeta', CVV2='$editarCVV2', FechaExpedicion='$editarFechaExpedicion', Direccion='$editarDireccion', Ciudad='$editarCiudad', Telefono='$editarTelefono', Departamento='$editarDepartamento', ZIP='$editarZIP', Pais='$editarPais' WHERE ID='$editarID'";
+            
+                // Prepare statement
+                $stmt = $conn->prepare($sql);
+            
+                // execute the query
+                $stmt->execute();
+            
+                // echo a message to say the UPDATE succeeded
+                echo $stmt->rowCount() . " Actualizado con exito";
+                }
+            catch(PDOException $e)
+                {
+                echo $sql . "<br>" . $e->getMessage();
+                console.log('sin exito');
                 }
         }
         
