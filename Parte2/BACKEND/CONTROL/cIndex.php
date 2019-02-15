40ca423df216bb1ebf5d65fb969eb5bbac3ccf77
Sergui <scastillonu@uniminuto.edu.co>
2019-02-14 19:50:52 -0500

    Update cIndex.php
    Actualice la pagina paa colocar el editar del crud

--- Parte2/BACKEND/CONTROL/cIndex.php
+++ Parte2/BACKEND/CONTROL/cIndex.php
@@ -12,8 +12,12 @@ else if(isset($_GET['idEliminar'])){
     $con=new conectar();
     $con->eliminar($_GET['idEliminar']);
 }
-else if(isset($_GET['actualizarID'])){
-    $con=new conectar();
-    $con->actualizar($_GET['actualizarID']);
-}
+else if(isset($_GET['editarID']) && isset($_GET['editarNombre']) && isset($_GET['editarApellido']) && isset($_GET['editarEmail']) && isset($_GET['editarTipoTarjeta'])
+&& isset($_GET['editarTitular']) && isset($_GET['editarNumeroTarjeta']) && isset($_GET['editarCVV2']) && isset($_GET['editarFechaExpedicion']) && isset($_GET['editarDireccion'])
+&& isset($_GET['editarCiudad']) && isset($_GET['editarDepartamento']) && isset($_GET['editarZIP']) && isset($_GET['editarPais']) && isset($_GET['editarTelefono'])){
+   echo('ha llehado a editar');
+    $conn=new conectar();
+    $conn->editar($_GET['editarID'],$_GET['editarNombre'],$_GET['editarApellido'],$_GET['editarEmail'],$_GET['editarTipoTarjeta'],$_GET['editarTitular'],$_GET['editarNumeroTarjeta'],$_GET['editarCVV2'],
+    $_GET['editarFechaExpedicion'],$_GET['editarDireccion'],$_GET['editarCiudad'],$_GET['editarDepartamento'],$_GET['editarZIP'],$_GET['editarPais'],$_GET['editarTelefono']);
 
+}
